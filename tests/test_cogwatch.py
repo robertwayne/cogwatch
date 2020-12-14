03c8b0cc7397d91348fa93a6babfaaadf675767e
Rob Wagner <13954303+robertwayne@users.noreply.github.com>
2020-12-14 01:56:52 -0500

    Added tests for bugfix a5f981a.

--- tests/test_cogwatch.py
+++ tests/test_cogwatch.py
@@ -13,18 +13,27 @@ def test_get_dotted_cog_path():
     # test the default path
     default_watcher = Watcher(c)
     default_path = default_watcher.get_dotted_cog_path('E:\\Examples\\example_bot\\commands\\cmd.py')
+    posix_path = default_watcher.get_dotted_cog_path('/usr/example_bot/commands/cmd.py')
 
     # test a nested path with duplicate names
     options_watcher = Watcher(c, cogs_path='example_bot/commands')
     nested_path = options_watcher.get_dotted_cog_path('E:\\Examples\\example_bot\\example_bot\\commands\\cmd.py')
+    posix_nested_path = options_watcher.get_dotted_cog_path('/usr/example_bot/example_bot/commands/cmd.py')
 
     # test deeper nests
     options_watcher.cogs_path = 'example_bot/even/deeper/commands'
     deep_nested_path = options_watcher.get_dotted_cog_path('E:\\Examples\\example_bot\\example_bot\\even\\deeper\\commands\\cmd.py')
+    posix_deep_nested_path = options_watcher.get_dotted_cog_path('/usr/example_bot/example_bot/even/deeper/commands/cmd.py')
 
     assert default_path == 'commands'
+    assert posix_path == 'commands'
+
     assert nested_path == 'example_bot.commands'
+    assert posix_nested_path == 'example_bot.commands'
+
     assert deep_nested_path == 'example_bot.even.deeper.commands'
+    assert posix_deep_nested_path == 'example_bot.even.deeper.commands'
+
 
 
 def test_get_dotted_cog_path_input():
@@ -47,6 +56,9 @@ def test_get_cog_name():
     default_watcher = Watcher(c)
 
     path = 'E:\\Examples\\example_bot\\commands\\cmd.py'
+    posix_path = '/usr/example_bot/commands/cmd.py'
     cog = default_watcher.get_cog_name(path)
+    posix_cog = default_watcher.get_cog_name(posix_path)
 
     assert cog == 'cmd'
+    assert posix_cog == 'cmd'
