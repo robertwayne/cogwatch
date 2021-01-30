75659762a3f304abd2395c88144c82d3c2f857f8
Rob <rob@robwagner.dev>
2021-01-30 16:21:35 -0500

    Slimmed tests down to take advantage of os.sep

--- tests/test_cogwatch.py
+++ tests/test_cogwatch.py
@@ -12,31 +12,19 @@ def test_get_dotted_cog_path():
 
     # test the default path
     default_watcher = Watcher(c)
-    default_path = default_watcher.get_dotted_cog_path("E:\\Examples\\example_bot\\commands\\cmd.py")
-    posix_path = default_watcher.get_dotted_cog_path("/usr/example_bot/commands/cmd.py")
+    path = default_watcher.get_dotted_cog_path("/usr/example_bot/commands/cmd.py")
 
     # test a nested path with duplicate names
     options_watcher = Watcher(c, path="example_bot/commands")
-    nested_path = options_watcher.get_dotted_cog_path("E:\\Examples\\example_bot\\example_bot\\commands\\cmd.py")
-    posix_nested_path = options_watcher.get_dotted_cog_path("/usr/example_bot/example_bot/commands/cmd.py")
+    nested_path = options_watcher.get_dotted_cog_path("/usr/example_bot/example_bot/commands/cmd.py")
 
     # test deeper nests
     options_watcher.path = "example_bot/even/deeper/commands"
-    deep_nested_path = options_watcher.get_dotted_cog_path(
-        "E:\\Examples\\example_bot\\example_bot\\even\\deeper\\commands\\cmd.py"
-    )
-    posix_deep_nested_path = options_watcher.get_dotted_cog_path(
-        "/usr/example_bot/example_bot/even/deeper/commands/cmd.py"
-    )
+    deep_nested_path = options_watcher.get_dotted_cog_path("/usr/example_bot/example_bot/even/deeper/commands/cmd.py")
 
-    assert default_path == "commands"
-    assert posix_path == "commands"
-
+    assert path == "commands"
     assert nested_path == "example_bot.commands"
-    assert posix_nested_path == "example_bot.commands"
-
     assert deep_nested_path == "example_bot.even.deeper.commands"
-    assert posix_deep_nested_path == "example_bot.even.deeper.commands"
 
 
 def test_get_dotted_cog_path_input():
@@ -45,23 +33,20 @@ def test_get_dotted_cog_path_input():
 
     # test invalid input backslash
     with pytest.raises(ValueError):
-        default_watcher.path = "example_bot\\commands"
-        default_watcher.get_dotted_cog_path("E:\\Examples\\example_bot\\example_bot\\commands\\cmd.py")
+        default_watcher.path = r"example_bot\commands"
+        default_watcher.get_dotted_cog_path(r"E:\Examples\example_bot\example_bot\commands\cmd.py")
 
     # test invalid input dotted
     with pytest.raises(ValueError):
         default_watcher.path = "example_bot.commands"
-        default_watcher.get_dotted_cog_path("E:\\Examples\\example_bot\\example_bot\\commands\\cmd.py")
+        default_watcher.get_dotted_cog_path(r"E:\Examples\example_bot\example_bot\commands\cmd.py")
 
 
 def test_get_cog_name():
     c = ClientMock()
     default_watcher = Watcher(c)
 
-    path = "E:\\Examples\\example_bot\\commands\\cmd.py"
-    posix_path = "/usr/example_bot/commands/cmd.py"
+    path = "/usr/example_bot/commands/cmd.py"
     cog = default_watcher.get_cog_name(path)
-    posix_cog = default_watcher.get_cog_name(posix_path)
 
     assert cog == "cmd"
-    assert posix_cog == "cmd"
