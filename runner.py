2c2e970da1a7f544061e855645fbec986ab57cf9
Rob <rob@robwagner.dev>
2021-02-15 17:23:06 -0500

    Added example runner script for testing.

---
+++ runner.py
@@ -0,0 +1,57 @@
+# This is a testing module which interfaces with the documentation examples. You will need to
+# have an environment variable called "COGWATCH_BOT_TOKEN" set in order for this to run. This
+# module must live in the root directory.
+#
+# You can run this file directly or with `poetry run example`.
+
+import asyncio
+import logging
+import os
+import sys
+
+from cogwatch import watch
+from discord.ext import commands
+from dotenv import load_dotenv
+
+try:
+    import uvloop
+except ImportError:
+    uvloop = None
+
+logging.basicConfig(level=logging.INFO)
+
+if sys.platform in {"linux", "macos"}:
+    uvloop.install()
+    logging.info("Using `uvloop` asyncio event loop.")
+
+load_dotenv()
+
+
+class ExampleRunner(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=".")
+
+    @watch(path="examples/commands", preload=True)
+    async def on_ready(self):
+        logging.info("Bot ready.")
+
+    async def on_message(self, message):
+        logging.info(message)
+
+        if message.author.bot:
+            return
+
+        await self.process_commands(message)
+
+
+async def main():
+    client = ExampleRunner()
+    await client.start(os.getenv("COGWATCH_BOT_TOKEN"))
+
+
+def __poetry_run():
+    asyncio.run(main())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
