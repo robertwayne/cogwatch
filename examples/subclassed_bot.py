c7eb6a70833f9ad695ec0fc10a45f0d84337b1ab
Rob <rob@robwagner.dev>
2021-02-15 17:02:12 -0500

    Added poetry script for running the subclassed bot.

--- examples/subclassed_bot.py
+++ examples/subclassed_bot.py
@@ -1,8 +1,15 @@
 import asyncio
+import os
 
 from discord.ext import commands
 
 from cogwatch import watch
+
+# This let's us easily load our bot token from a .env
+# file in the root directory so it is never exposed.
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class ExampleBot(commands.Bot):
@@ -22,7 +29,13 @@ class ExampleBot(commands.Bot):
 
 async def main():
     client = ExampleBot()
-    await client.start("YOUR_TOKEN_GOES_HERE")
+    await client.start(os.getenv("COGWATCH_BOT_TOKEN"))
+
+
+# This function is specifically for running from a poetry script.
+# You do not need this in your own bot, you can remove these 2 lines.
+def __poetry_run():
+    asyncio.run(main())
 
 
 if __name__ == "__main__":
