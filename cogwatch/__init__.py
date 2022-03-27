181b44f67f745f837b59f5641c0dc469826d0050
Rob <rob@sombia.com>
2022-03-27 15:07:39 -0400

    Update meta data

--- cogwatch/__init__.py
+++ cogwatch/__init__.py
@@ -3,15 +3,14 @@ dpymenus -- Simplified menus for discord.py developers.
 """
 
 __title__ = "cogwatch"
-__author__ = "Rob Wagner <rob@robwagner.dev>"
+__author__ = "Rob Wagner <rob@sombia.com>"
 __license__ = "MIT"
-__copyright__ = "Copyright 2020-2021 Rob Wagner"
+__copyright__ = "Copyright 2020-2022 Rob Wagner"
 __version__ = "3.0.0"
 
 import logging
 
 from cogwatch.cogwatch import Watcher, watch
-
 
 logger = logging.getLogger("cogwatch")
 logger.addHandler(logging.NullHandler())
