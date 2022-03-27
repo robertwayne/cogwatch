09eea25a5e91936597b121da3cc929c96a3c56e0
Rob <rob@sombia.com>
2022-03-27 15:12:35 -0400

    Set version for alpha release on PyPi

--- cogwatch/__init__.py
+++ cogwatch/__init__.py
@@ -6,7 +6,7 @@ __title__ = "cogwatch"
 __author__ = "Rob Wagner <rob@sombia.com>"
 __license__ = "MIT"
 __copyright__ = "Copyright 2020-2022 Rob Wagner"
-__version__ = "3.0.0"
+__version__ = "3.0.0a"
 
 import logging
 
