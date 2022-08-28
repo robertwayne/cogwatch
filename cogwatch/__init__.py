2ad0b5dbd8e813dc79008ced94d0c7061754e757
Rob Wagner <rob@sombia.com>
2022-08-28 00:01:50 -0400

    Minor version bump.

--- cogwatch/__init__.py
+++ cogwatch/__init__.py
@@ -6,6 +6,6 @@ __title__ = "cogwatch"
 __author__ = "Rob Wagner <rob@sombia.com>"
 __license__ = "MIT"
 __copyright__ = "Copyright 2020-2022 Rob Wagner"
-__version__ = "3.0.1"
+__version__ = "3.1.0"
 
 from cogwatch.cogwatch import Watcher, watch
