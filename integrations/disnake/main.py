800bd6b9d73a7d115fcb349aa3dda52767855b6a
Rob Wagner <rob@sombia.com>
2023-04-02 12:57:40 -0400

    Fix disnake integration test

--- integrations/disnake/main.py
+++ integrations/disnake/main.py
@@ -6,15 +6,15 @@ import asyncio
 import logging
 import os
 
-import discord
-from discord.ext import commands
+import disnake
+from disnake.ext import commands
 from dotenv import load_dotenv
 
 from cogwatch import Watcher, watch
 
 load_dotenv()
 
-intents = discord.Intents.default()
+intents = disnake.Intents.default()
 intents.message_content = True
 
 
